ca206dfa452817ec1ecbea8f784f018278cbfde2
William Schwindt <williammschwindt@gmail.com>
2020-03-11 10:29:51 -0700

    william-schwndt

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,4 +3,5 @@ import axios from 'axios';
 export const fetchShow = () => {
     return axios.get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(res => res) 
+      .catch(err => err)
   }
